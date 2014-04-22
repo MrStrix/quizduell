63f54399c763393a5838d1b61c0d56bca391434d
strix <strix@schwyzerstaern.ch>
2014-04-22 21:54:13 +0200

    questions are read from xml

--- src/quizduell/Duel.java
+++ src/quizduell/Duel.java
@@ -1,14 +1,46 @@
 package quizduell;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 public class Duel {
 	private ArrayList<User> users = new ArrayList<User>();
-
+	private ArrayList<Question> questions = new ArrayList<Question>();
+	
 	public Duel(User a, User b) {
 		super();
 		this.users.add(a);
 		this.users.add(b);
+				
+		try {
+   
+			// create JAXB context and initializing Marshaller
+			JAXBContext jaxbContext = JAXBContext.newInstance(Question.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		   
+			// specify the location and name of xml file to be read
+			File XMLfile = new File("/home/strix/myquestions.xml");
+		   
+			// this will create Java object - country from the XML file
+			Question countryIndia = (Question) jaxbUnmarshaller.unmarshal(XMLfile);
+		
+			System.out.println("Country Name: "+countryIndia.toString());
+
+		
+		} catch (JAXBException e) {
+			// some exception occured
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Question getQuestion() {
+		return this.questions.get(0);
 	}
 	
 	public ArrayList<User> getUsers() {
--- src/quizduell/Question.java
+++ src/quizduell/Question.java
@@ -1,5 +1,98 @@
 package quizduell;
 
+import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+//Below annotation defines root element of XML file
+@XmlRootElement
+//You can define order in which elements will be created in XML file
+//Optional
+@XmlType(propOrder = { "questionName", "correctAnswer", "answerList" })
 public class Question {
 
+	private String questionName;
+	private ArrayList<String> answerList = new ArrayList<String>();
+	private int correctAnswer;
+	
+	
+	public Question() {
+		super();
+	}
+	
+	public boolean verify(int answer) {
+		if(answer == this.correctAnswer)
+			return true;
+		else
+			return false;
+	}
+	
+	public String getAnswerList1() {
+		String answers="";
+		for( int i = 0; i < this.answerList.size(); i++ ) {
+			answers += i + ") " + this.answerList.get(i) + "\n";
+		}
+		return answers;
+	}
+	
+	
+	
+	/**
+	 * @param questionName the questionName to set
+	 */
+	@XmlElement
+	public void setQuestionName(String questionName) {
+		this.questionName = questionName;
+	}
+
+	/**
+	 * @param correctAnswer the correctAnswer to set
+	 */
+	@XmlElement  
+	public void setCorrectAnswer(int correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+
+	// XmLElementWrapper generates a wrapper element around XML representation  
+	@XmlElementWrapper(name = "answerList")  
+	// XmlElement sets the name of the entities in collection  
+	@XmlElement(name = "answer")  
+	public void setAnswerList(ArrayList<String> answerList) {
+		this.answerList = answerList;
+	}
+	
+	/**
+	 * @return the question
+	 */
+	public String getQuestion() {
+		return questionName;
+	}
+
+	/**
+	 * @return the answers
+	 */
+	public ArrayList<String> getAnswerList() {
+		return answerList;
+	}
+
+	/**
+	 * @return the correctAnswer
+	 */
+	public int getCorrectAnswer() {
+		return correctAnswer;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Question [questionName=" + questionName + ", answerList="
+				+ answerList + ", correctAnswer=" + correctAnswer + "]";
+	}
+	
+	
+	
 }
